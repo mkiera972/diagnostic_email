3de9c0adb08961052464280c692ce7f4f1faf7d5
jérémy angele <jeremyangele@gmail.com>
2022-11-06 17:40:01 -0400

    init api mail diagnostique

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const Mailjet = require('node-mailjet');
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/send', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log(req.body)
+
+    const mailjet = Mailjet.apiConnect(
+        "3f3dae485ab2a5556d23c2afce4d39dd",
+        "339fc46f8ceb6810376dc80a8cf533cd",
+    );
+    
+    const request = mailjet
+            .post('send', { version: 'v3.1' })
+            .request({
+              Messages: [
+                {
+                  From: {
+                    Email: "contact@maison-des-experts-immobiliers.fr",
+                    Name: "Mailjet Pilot"
+                  },
+                  To: [
+                    {
+                      Email: "contact@wiwaks.com",
+                      Name: "passenger 1"
+                    }
+                  ],
+                  Subject: "Your email flight plan!",
+                  TextPart: "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+                  HTMLPart: req.body.body
+                }
+              ]
+            })
+    
+    request
+        .then((result) => {
+            console.log(result.body)
+        })
+        .catch((err) => {
+            console.log(err.statusCode)
+        })
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+module.exports = app;
